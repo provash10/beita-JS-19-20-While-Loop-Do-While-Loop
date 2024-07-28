6a1a087715bf4047fcab21b2113a8878e148e330
provash10 <provash20cb@gmail.com>
2024-07-28 10:18:27 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// var i = 5 ;
+// while (i <10){
+//     console.log(i, "<br>");
+//     i--;
+// }
+
+// 20 Do While Loop
+
+var i = 50;
+
+do {
+    document.write( i, "<br>");
+}while(i >= 10);
